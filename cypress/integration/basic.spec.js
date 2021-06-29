ec96b85c9a8fe717a304a7e31461407bcd07892a
JefersonLima <jefersonlimadesouza@hotmail.com.br>
2021-06-29 18:58:24 -0400

    Aula 8 Localizar e interagir com elemento
    inspecionando um Site.

--- cypress/integration/basic.spec.js
+++ cypress/integration/basic.spec.js
@@ -2,25 +2,34 @@
 
 describe('Cypress basics', () => {
     it('Should visit a page and assert title', () => {
-    cy.visit('https://wcaquino.me/cypress/componentes.html')
-    
-    cy.title().should('be.equal', 'Campo de Treinamento')
-    cy.title().should('contain', 'Campo')
+        cy.visit('https://wcaquino.me/cypress/componentes.html')
+        
+        cy.title().should('be.equal', 'Campo de Treinamento')
+        cy.title().should('contain', 'Campo')
 
-    cy.title()
-        .should('be.equal', 'Campo de Treinamento')
-        .should('contain', 'Campo')
+        cy.title()
+            .should('be.equal', 'Campo de Treinamento')
+            .should('contain', 'Campo')
 
-    cy.title()
-        .should('be.equal', 'Campo de Treinamento')
-        .and('contain', 'Campo')
+        cy.title()
+            .should('be.equal', 'Campo de Treinamento')
+            .and('contain', 'Campo')
 
-    // TODO imprimir o log no console
-    // TODO esvrever o log em um campo de texto
-
-
-
+        // TODO imprimir o log no console
+        // TODO esvrever o log em um campo de texto
     })
+
+    it.only('Should find and interact with an element', () => {
+        cy.visit('https://wcaquino.me/cypress/componentes.html')
+        
+        //cy.get('nao existe')
+        cy.get('#buttonSimple')
+        .click()
+        .should('have.value', 'Obrigado!')
+
+        expect
+    })
+    
 })
 
 
