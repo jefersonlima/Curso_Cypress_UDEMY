a4bf069198574f6e2d8b73040e82dd7605bc8b92
JefersonLima <jefersonlimadesouza@hotmail.com.br>
2021-06-29 13:49:12 -0400

    Aula 7 Acessar uma página

---
+++ cypress/integration/basic.spec.js
@@ -0,0 +1,26 @@
+/// <reference types="cypress" />
+
+describe('Cypress basics', () => {
+    it('Should visit a page and assert title', () => {
+    cy.visit('https://wcaquino.me/cypress/componentes.html')
+    
+    cy.title().should('be.equal', 'Campo de Treinamento')
+    cy.title().should('contain', 'Campo')
+
+    cy.title()
+        .should('be.equal', 'Campo de Treinamento')
+        .should('contain', 'Campo')
+
+    cy.title()
+        .should('be.equal', 'Campo de Treinamento')
+        .and('contain', 'Campo')
+
+    // TODO imprimir o log no console
+    // TODO esvrever o log em um campo de texto
+
+
+
+    })
+})
+
+
