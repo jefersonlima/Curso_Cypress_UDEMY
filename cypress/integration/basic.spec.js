cf1d6046ac640e23763ae060dcfb69486494201a
JefersonLima <jefersonlimadesouza@hotmail.com.br>
2021-07-01 21:01:32 -0400

    Aula 28 Reutilizando o Titulo
    pagando divida resolvendo TODO pendentes

--- cypress/integration/basic.spec.js
+++ cypress/integration/basic.spec.js
@@ -20,12 +20,23 @@ describe('Cypress basics', () => {
             console.log(title)
         })
 
-        cy.title().should(title => {
+        let syncTitle
+
+        cy.title().then(title => {
             console.log(title)
+
+            cy.get('#formNome').type(title)
+            syncTitle = title
         })
 
-        // TODO imprimir o log no console
-        // TODO esvrever o log em um campo de texto
+        cy.get('[data-cy=dataSobrenome]').then($el => {
+            $el.val(syncTitle)
+        })
+        
+        cy.get('#elementosForm\\:sugestoes').then($el => {
+            cy.wrap($el).type(syncTitle)
+        })
+
     })
 
     it('Should find and interact with an element', () => {
