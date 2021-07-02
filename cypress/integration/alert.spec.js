473545706e59ccc4e40f81db94edec3d70d8cfe8
JefersonLima <jefersonlimadesouza@hotmail.com.br>
2021-07-02 06:32:06 -0400

    Aula 33 Confirm

--- cypress/integration/alert.spec.js
+++ cypress/integration/alert.spec.js
@@ -19,13 +19,40 @@ describe('Work with basic elements', () => {
         })
     })
 
-    it.only('Alert com Mock...', () =>{
+    it('Alert com Mock...', () =>{
         const stub = cy.stub().as('Alerta')
         cy.on('window:alert',stub)
         cy.get('#alert').click().then(() => {
             expect(stub.getCall(0)).to.be.calledWith('Alert Simples')
         })
         
+    })
+
+    it('Confirm...', () =>{
+        cy.on('window:confirm', msg => {
+            //console.log(msg)
+            expect(msg).to.be.equal('Confirm Simples')
+        })
+
+        cy.on('window:alert', msg => {
+            //console.log(msg)
+            expect(msg).to.be.equal('Confirmado')
+        })
+
+        cy.get('#confirm').click()
+    })
+
+    it.only('Deny...', () =>{
+        cy.on('window:confirm', msg => {
+            expect(msg).to.be.equal('Confirm Simples')
+            return false
+        })
+
+        cy.on('window:alert', msg => {
+            expect(msg).to.be.equal('Negado')
+        })
+        
+        cy.get('#confirm').click()
     })
 
 })
