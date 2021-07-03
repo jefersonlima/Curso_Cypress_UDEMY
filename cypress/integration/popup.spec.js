427334fb01cd7ace8c471582916232c73aece975
JefersonLima <jefersonlimadesouza@hotmail.com.br>
2021-07-02 20:17:24 -0400

    Aula 39 Popup via Links

--- cypress/integration/popup.spec.js
+++ cypress/integration/popup.spec.js
@@ -30,4 +30,32 @@ describe('Work with Popup', () => {
             .should('be.called')
     })
 
+    describe.only('With links...', () => {
+        beforeEach(() => {
+            cy.visit('https://wcaquino.me/cypress/componentes.html')
+        })
+        
+        it('Check popup url', () => {
+            cy.contains('Popup2')
+                .should('have.prop', 'href')
+                .and('equal', 'https://wcaquino.me/cypress/frame.html')
+        })
+
+        it('Should acess popup dinamically', () => {
+            cy.contains('Popup2').then($a => {
+                const href = $a.prop('href')
+                cy.visit(href)
+
+                cy.get('#tfield').type('Funciona')
+            })
+        })
+
+        it('Should force link on same page', () => {
+            cy.contains('Popup2')
+                .invoke('removeAttr', 'target')
+                .click()
+                cy.get('#tfield').type('Funciona')
+        })
+    })
+
 })
