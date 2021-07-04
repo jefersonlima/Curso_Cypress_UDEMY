5b007feab570e58dcd5d092687ec59210cac2d3e
JefersonLima <jefersonlimadesouza@hotmail.com.br>
2021-07-04 18:05:12 -0400

    Aula 56 Comandos de conta

--- cypress/integration/barriga/functional.spec.js
+++ cypress/integration/barriga/functional.spec.js
@@ -1,32 +1,25 @@
 /// <reference types="cypress" />
 
 import loc from '../../support/locators'
+import '../../support/commandsContas'
 
 describe('Should test at a functional level', () => {
     //vai execultar uma vez antes de todos os testes
     before(() => {
         cy.login('jefersonlimadesouza@hotmail.com.br', '11080822')
         cy.resetApp()
-        // cy.visit('http://barrigareact.wcaquino.me')
-        // cy.get(loc.LOGIN.USER).type('jefersonlimadesouza@hotmail.com.br')
-        // cy.get(loc.LOGIN.PASSWORD).type('11080822')
-        // cy.get(loc.LOGIN.BTN_LOGIN).click()
-        // //cy.get('.toast-message').should('exist')
-        // cy.get(loc.MESSAGE).should('contain', 'Bem vindo')
     })
 
     it('Should create an acount', () => {
-        cy.get(loc.MENU.SETTINGS).click()
-        cy.get(loc.MENU.CONTAS).click()
-        cy.get(loc.CONTAS.NOME).type('Conta test create')
-        cy.get(loc.CONTAS.BTN_SALVAR).click()
-        //cy.get('.toast-success > .toast-message').should('exist')
+        cy.acessarMenuConta()
+        cy.inserirConta('Conta test create')
+ 
         cy.get(loc.MESSAGE).should('contain', 'Conta inserida com sucesso')
     }) 
 
     it('Alter acount', () => {
-        cy.get(loc.MENU.SETTINGS).click()
-        cy.get(loc.MENU.CONTAS).click()
+        cy.acessarMenuConta()
+
         cy.xpath(loc.CONTAS.XP_BTN_ALTERAR).click()
  
         cy.get(loc.CONTAS.NOME)
