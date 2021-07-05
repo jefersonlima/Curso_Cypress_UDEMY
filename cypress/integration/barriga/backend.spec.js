aaf032475d64c1a18835c4478daf0612d87870f5
JefersonLima <jefersonlimadesouza@hotmail.com.br>
2021-07-05 19:58:46 -0400

    Aula 64 Inserir conta

--- cypress/integration/barriga/backend.spec.js
+++ cypress/integration/barriga/backend.spec.js
@@ -19,6 +19,23 @@ describe('Should test at a functional level', () => {
                 senha: "11080822"
             }
         }).its('body.token').should('not.be.empty')
+            .then(token => {
+                cy.request({
+                    url: 'https://barrigarest.wcaquino.me/contas',
+                    method: 'POST',
+                    headers: { Authorization: `JWT ${token}`},
+                    //headers: { Authorization: `bearer ${token}`},//novas versoes
+                    body: {
+                       nome: "Conta via rest" 
+                    }
+                }).as('response')
+            })
+        
+            cy.get('@response'). then(res => {
+                expect(res.status).to.be.equal(201)
+                expect(res.body).to.have.property('id')
+                expect(res.body).to.have.property('nome', 'Conta via rest')
+            })
     }) 
 
     it('Alter acount', () => {
