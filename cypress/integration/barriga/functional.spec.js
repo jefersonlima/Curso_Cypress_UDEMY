e55415137766b9352d81ae5f8c53571b74fd4f88
JefersonLima <jefersonlimadesouza@hotmail.com.br>
2021-07-05 14:08:37 -0400

    Aula 61 Gerenciamento de massa

--- cypress/integration/barriga/functional.spec.js
+++ cypress/integration/barriga/functional.spec.js
@@ -7,9 +7,15 @@ describe('Should test at a functional level', () => {
     //vai execultar uma vez antes de todos os testes
     before(() => {
         cy.login('jefersonlimadesouza@hotmail.com.br', '11080822')
-        cy.resetApp()
-        cy.wait(2000)
+        cy.wait(1000)
     })
+    
+    beforeEach(() => {
+        cy.get(loc.MENU.HOME).click()
+        cy.resetApp()
+    })
+
+
     
     it('Should create an acount', () => {
         cy.acessarMenuConta()
@@ -21,11 +27,11 @@ describe('Should test at a functional level', () => {
     it('Alter acount', () => {
         cy.acessarMenuConta()
 
-        cy.xpath(loc.CONTAS.FN_XP_BTN_ALTERAR('Conta test create')).click()
+        cy.xpath(loc.CONTAS.FN_XP_BTN_ALTERAR('Conta para alterar')).click()
  
         cy.get(loc.CONTAS.NOME)
             .clear()
-            .type('Conta test create')
+            .type('Conta para alterar')
         cy.get(loc.CONTAS.BTN_SALVAR).click()
         cy.get(loc.MESSAGE).should('contain', 'Conta atualizada com sucesso!')
     })
@@ -33,7 +39,7 @@ describe('Should test at a functional level', () => {
     it('Should not create an account with same name', () => {
         cy.acessarMenuConta()
 
-        cy.get(loc.CONTAS.NOME).type('Conta test create')
+        cy.get(loc.CONTAS.NOME).type('Conta mesmo nome')
         cy.get(loc.CONTAS.BTN_SALVAR).click()
         cy.get(loc.MESSAGE).should('contain', 'code 400')
     })
@@ -44,28 +50,46 @@ describe('Should test at a functional level', () => {
         cy.get(loc.MOVIMENTACAO.DESCRICAO).type('Desc')
         cy.get(loc.MOVIMENTACAO.VALOR).type('123')
         cy.get(loc.MOVIMENTACAO.INTERESSADO).type('Inter')
-        cy.get(loc.MOVIMENTACAO.CONTA).select('Conta test create')
+        cy.get(loc.MOVIMENTACAO.CONTA).select('Conta para movimentacoes')
         cy.get(loc.MOVIMENTACAO.STATUS).click()
-
+        
         cy.get(loc.MOVIMENTACAO.BTN_SALVAR).click()
-
+        
         cy.get(loc.MESSAGE).should('contain', 'sucesso')
-
+        
         cy.get(loc.EXTRATO.LINHAS).should('have.length', 7)
-
+        
         cy.xpath(loc.EXTRATO.FN_XP_BUSCA_ELEMENTO('Desc','123'))
-            .should('exist')
+        .should('exist')
     })
-
-    it('Should get balance', () => {
-        console.log(loc.SALDO.FN_XP_SALDO_CONTA('Conta test create'))
+    
+    it.only('Should get balance', () => {
+        cy.resetApp()
         cy.get(loc.MENU.HOME).click()
-        cy.xpath(loc.SALDO.FN_XP_SALDO_CONTA('Conta test create')).should('contain', '123')
+        cy.xpath(loc.SALDO.FN_XP_SALDO_CONTA('Conta para saldo')).should('contain', '534,00')
+        
+        cy.get(loc.MENU.EXTRATO).click()
+        
+        cy.xpath(loc.EXTRATO.FN_XP_ALTERAR_ELEMENTO('Movimentacao 1, calculo saldo')).click()
+        
+        cy.get(loc.MOVIMENTACAO.DESCRICAO).should('have.value', 'Movimentacao 1, calculo saldo')
+        
+        cy.get(loc.MOVIMENTACAO.STATUS).click()
+        
+        cy.get(loc.MOVIMENTACAO.BTN_SALVAR).click()
+        
+        cy.get(loc.MESSAGE).should('contain', 'sucesso')
+        //Colocado esse wait prq estava execultando meuito rapido e não dava tempo de pegar o retorno
+        cy.wait(1000)
+        cy.get(loc.MENU.HOME).click()
+        cy.xpath(loc.SALDO.FN_XP_SALDO_CONTA('Conta para saldo')).should('contain', '4.034,00')
+        
     })
 
     it('should remove a transaction', () => {
         cy.get(loc.MENU.EXTRATO).click()
-        cy.xpath(loc.EXTRATO.FN_XP_REMOVER_ELEMENTO('Desc')).click()
+        cy.xpath(loc.EXTRATO.FN_XP_REMOVER_ELEMENTO('Movimentacao para exclusao'))
+            .click()
 
         cy.get(loc.MESSAGE).should('contain', 'sucesso')
     })
