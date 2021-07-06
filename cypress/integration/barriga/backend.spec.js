4d1eccdd3adbdd33dd2e04a0d76c7d1632c130e9
JefersonLima <jefersonlimadesouza@hotmail.com.br>
2021-07-06 06:30:31 -0400

    Aula 70 Saldo

--- cypress/integration/barriga/backend.spec.js
+++ cypress/integration/barriga/backend.spec.js
@@ -99,7 +99,53 @@ describe('Should test at a functional level', () => {
     })
     
     it('Should get balance', () => {
+        cy.request({
+            url: '/saldo',
+            method: 'GET',
+            headers: { Authorization: `JWT ${token}`}
+        }).then(res => {
+            let saldoConta = null
+            res.body.forEach(c => {
+                if(c.conta === 'Conta para saldo') saldoConta = c.saldo
+            })
+            expect(saldoConta).to.be.equal('534.00')
+        })
         
+        cy.request({
+            method: 'GET',
+            url: '/transacoes',
+            headers: { Authorization: `JWT ${token}`},
+            qs: { descricao: 'Movimentacao 1, calculo saldo'}
+        }).then(res => {
+            console.log(res.body[0])
+            cy.request({
+                url: `/transacoes/${res.body[0].id}`,
+                method: 'PUT',
+                headers: { Authorization: `JWT ${token}`},
+                body: {
+                    status: true,
+                    data_transacao: Cypress.moment(res.body[0].data_transacao).format('DD/MM/YYYY'),
+                    data_pagamento: Cypress.moment(res.body[0].data_pagamento).format('DD/MM/YYYY'),
+                    descricao: res.body[0].descricao,
+                    envolvido: res.body[0].envolvido,
+                    valor: res.body[0].valor,
+                    conta_id: res.body[0].conta_id
+
+                }
+            }).its('status').should('be.equal', 200)
+        })
+
+        cy.request({
+            url: '/saldo',
+            method: 'GET',
+            headers: { Authorization: `JWT ${token}`}
+        }).then(res => {
+            let saldoConta = null
+            res.body.forEach(c => {
+                if(c.conta === 'Conta para saldo') saldoConta = c.saldo
+            })
+            expect(saldoConta).to.be.equal('4034.00')
+        })
     })
 
     it('should remove a transaction', () => {
