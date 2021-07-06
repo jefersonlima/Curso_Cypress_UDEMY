87683501b061deb3efd119125357446b48646ad3
JefersonLima <jefersonlimadesouza@hotmail.com.br>
2021-07-05 21:28:13 -0400

    Aula 65 getToken

--- cypress/integration/barriga/backend.spec.js
+++ cypress/integration/barriga/backend.spec.js
@@ -1,8 +1,13 @@
 /// <reference types="cypress" />
 
+import '../../support/commands'
 describe('Should test at a functional level', () => {
+    let token
     before(() => {
-
+        cy.getToken('jefersonlimadesouza@hotmail.com.br', '11080822')
+            .then(tkn => {
+                token = tkn
+            })
     })
     
     beforeEach(() => {
@@ -10,32 +15,23 @@ describe('Should test at a functional level', () => {
     })
     
     it('Should create an acount', () => {
-        cy.request({
-            method: 'POST',
-            url: 'https://barrigarest.wcaquino.me/signin',
-            body: {
-                email: "jefersonlimadesouza@hotmail.com.br",
-                redirecionar: false,
-                senha: "11080822"
-            }
-        }).its('body.token').should('not.be.empty')
-            .then(token => {
-                cy.request({
-                    url: 'https://barrigarest.wcaquino.me/contas',
-                    method: 'POST',
-                    headers: { Authorization: `JWT ${token}`},
-                    //headers: { Authorization: `bearer ${token}`},//novas versoes
-                    body: {
-                       nome: "Conta via rest" 
-                    }
-                }).as('response')
-            })
         
-            cy.get('@response'). then(res => {
-                expect(res.status).to.be.equal(201)
-                expect(res.body).to.have.property('id')
-                expect(res.body).to.have.property('nome', 'Conta via rest')
-            })
+        cy.request({
+            url: 'https://barrigarest.wcaquino.me/contas',
+            method: 'POST',
+            headers: { Authorization: `JWT ${token}`},
+            //headers: { Authorization: `bearer ${token}`},//novas versoes
+            body: {
+                nome: "Conta via rest" 
+            }
+        }).as('response')
+            
+        
+        cy.get('@response'). then(res => {
+            expect(res.status).to.be.equal(201)
+            expect(res.body).to.have.property('id')
+            expect(res.body).to.have.property('nome', 'Conta via rest')
+        })
     }) 
 
     it('Alter acount', () => {
